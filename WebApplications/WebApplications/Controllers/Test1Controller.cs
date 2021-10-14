1230d8e1f1c7623c9b8059570901daaf441eeef7
Samatcha <40911697+kiksaa@users.noreply.github.com>
2021-10-14 11:47:45 +0700

    test1

--- WebApplications/WebApplications/Controllers/Test1Controller.cs
+++ WebApplications/WebApplications/Controllers/Test1Controller.cs
@@ -26,12 +26,19 @@ namespace WebApplications.Controllers
         }
         public ActionResult Calculate(Test1 model)
         {
-            float simpleInteresrt = (float)(0.5f * model.base1 * model.height);
-            StringBuilder sbInterest = new StringBuilder();
-            sbInterest.Append("<b>base1 :</b> " + model.base1 + "<br/>");
-            sbInterest.Append("<b>height :</b> " + model.height + "<br/>");
-            sbInterest.Append("<b>Numder Digit :</b> " + simpleInteresrt);
-            return Content(sbInterest.ToString());
+            try
+            {
+                float simpleInteresrt = (float)(0.5f * model.base1 * model.height);
+                StringBuilder sbInterest = new StringBuilder();
+                sbInterest.Append("<b>base1 :</b> " + model.base1 + "<br/>");
+                sbInterest.Append("<b>height :</b> " + model.height + "<br/>");
+                sbInterest.Append("<b>Numder Digit :</b> " + simpleInteresrt);
+                return Content(sbInterest.ToString());
+            }
+            catch
+            {
+                return View("Error");
+            }
         }
         public ActionResult clear()
         {
@@ -48,42 +55,41 @@ namespace WebApplications.Controllers
         }
         public ActionResult Calculate3(Test3 model)
         {
-            StringBuilder sbInterest = new StringBuilder();
-            if (model.citizen_id.Length == null)
+            try
             {
-                sbInterest.Append("<b>success :</b> " + "fales" + "<br/>");
-                sbInterest.Append("<b>error_code :</b> " + "001" + " <br/>");
-                sbInterest.Append("<b>error_msg :</b> " + "citizen_id require" + " <br/>");
-                string simpleInteresrt = null;
-                sbInterest.Append("<b>Interest :</b> " + simpleInteresrt);
-            }
-            int sum = 0;
-            if (model.citizen_id.Length == 13)
-            {
-                for (int i = 0; i < model.citizen_id.Length - 1; i++)
+                StringBuilder sbInterest = new StringBuilder();
+                int sum = 0;
+                if (model.citizen_id.Length == 13)
                 {
-                    int n = Int32.Parse(model.citizen_id[i].ToString());
-                    sum = sum + ((13 - i) * n);
+                    for (int i = 0; i < model.citizen_id.Length - 1; i++)
+                    {
+                        int n = Int32.Parse(model.citizen_id[i].ToString());
+                        sum = sum + ((13 - i) * n);
+                    }
+                    int mod11 = sum % 11;
+                    int subtract11 = 11 - mod11;
+                    int simpleInteresrt1 = subtract11;
+                    sbInterest.Append("<b>citizen_id :</b> " + model.citizen_id + " <br/>");
+                    sbInterest.Append("<b>success :</b> " + "true" + "<br/>");
+                    sbInterest.Append("<b>error_code :</b> " + "200" + " <br/>");
+                    sbInterest.Append("<b>error_msg :</b> " + "" + " <br/>");
+                    sbInterest.Append("<b>Number Digit :</b> " + simpleInteresrt1);
                 }
-                int mod11 = sum % 11;
-                int subtract11 = 11 - mod11;
-                int simpleInteresrt1 = subtract11;
-                sbInterest.Append("<b>citizen_id :</b> " + model.citizen_id + " <br/>");
-                sbInterest.Append("<b>success :</b> " + "true" + "<br/>");
-                sbInterest.Append("<b>error_code :</b> " + "200" + " <br/>");
-                sbInterest.Append("<b>error_msg :</b> " + "" + " <br/>");
-                sbInterest.Append("<b>Number Digit :</b> " + simpleInteresrt1);
+                if (model.citizen_id.Length != 13)
+                {
+                    sbInterest.Append("<b>citizen_id :</b> " + model.citizen_id + " <br/>");
+                    sbInterest.Append("<b>success :</b> " + "fales" + "<br/>");
+                    sbInterest.Append("<b>error_code :</b> " + "001" + " <br/>");
+                    sbInterest.Append("<b>error_msg :</b> " + "citizen_id invalid" + " <br/>");
+                    string simpleInteresrt = null;
+                    sbInterest.Append("<b>Number Digit :</b> " + simpleInteresrt);
+                }
+                return Content(sbInterest.ToString());
             }
-            if (model.citizen_id.Length != 13)
+            catch
             {
-                sbInterest.Append("<b>citizen_id :</b> " + model.citizen_id + " <br/>");
-                sbInterest.Append("<b>success :</b> " + "fales" + "<br/>");
-                sbInterest.Append("<b>error_code :</b> " + "001" + " <br/>");
-                sbInterest.Append("<b>error_msg :</b> " + "citizen_id invalid" + " <br/>");
-                string simpleInteresrt = null;
-                sbInterest.Append("<b>Number Digit :</b> " + simpleInteresrt);
+                return View("ErrorID");
             }
-            return Content(sbInterest.ToString());
         }
     }
 }
--- WebApplications/WebApplications/Models/Test3.cs
+++ WebApplications/WebApplications/Models/Test3.cs
@@ -8,14 +8,7 @@ namespace WebApplications.Models
 {
     public class Test3
     {
-        /*public int id { get; set; }*/
         [DisplayName("หมายเลขบัตรประชาชน")]
         public string citizen_id { get; set; }
-        /*[DisplayName("success")]
-        public string success { get; set; }
-        [DisplayName("error_code")]
-        public string error_code { get; set; }
-        [DisplayName("error_msg")]
-        public string error_msg { get; set; }*/
     }
 }
